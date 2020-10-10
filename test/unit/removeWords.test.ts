5de715d1b0ae45176b1722537f4336ce57017432
Rogério Araújo <rogerio.araujo@mail.com>
2020-10-10 15:41:13 -0300

    feat: add some test to addWords and removeWords

---
+++ test/unit/removeWords.test.ts
@@ -0,0 +1,25 @@
+import assert = require('assert');
+
+import { Profanity } from "../../src/index";
+
+describe('removeWords tests', () => {
+  it("should return false because remove word from list of profane words", () => {
+    const profanity = new Profanity();
+    const actual = false;
+    const spected = profanity.removeWords("ass").isProfane("ass");
+
+    assert.strictEqual(actual, spected);
+  });
+
+  it("should ignore words remov from list of profane words", function () {
+    const profanity = new Profanity("oh shit, you are a asshole. go hell", {
+      language: "en-us",
+    });
+
+    const actual = "oh shit, you are a asshole. go ****";
+    const spected = profanity.removeWords("asshole", "shit").censor();
+
+    assert.strictEqual(actual, spected);
+    return
+  });
+});
